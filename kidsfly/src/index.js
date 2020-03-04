cfeb99f97b3de06e2872d14f514bff03a2e9bd7d
john ruble <jrockwv@gmail.com>
2020-03-03 22:06:48 -0500

    lets merge

--- kidsfly/src/index.js
+++ kidsfly/src/index.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import TripForm from './components/TripForm'
-import SubForm from './components/SubForm'
 
 
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+ReactDOM.render(<Router>
+  <App />
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
